16c2320c5ea4149f8fc05e5c83de18263689858d
jules <julia.sinclair.workemail@gmail.com>
2025-09-08 14:20:00 -0400

    fixed issues with multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,17 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
         int multipleCounter = 0;
-        for  (int i = 0; i <= 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        for  (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
                 multipleCounter++;
             }
         }
-
-        System.out.println(multipleCounter);
+        return multipleCounter;
     }
 }
