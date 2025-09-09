a317724f1d2ec43215651d09909ff8d1030044d4
jules <julia.sinclair.workemail@gmail.com>
2025-09-08 20:54:12 -0400

    reduce exercise

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = reduce(100);
+        System.out.println(steps);
+    }
+
+    static int reduce(int n) {
+        int i = n;
+        int stepsToZero = 0;
+
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else {
+                i = i - 1;
+            }
+            stepsToZero++;
+        }
+
+        return stepsToZero;
+    }
+}
